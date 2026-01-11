21a050dd5f90b7533741df811d241b27047439f5
Pulkit0707 <pulkit0707chawla@gmail.com>
2026-01-11 14:41:01 +0530

    call app from main.cpp

--- FoodDeliveryApp/main.cpp
+++ FoodDeliveryApp/main.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
+#include "App.h"
+#include "models/User.h"
+#include "models/Restraunt.h"
 using namespace std;
 
-int main(){
+int main(){ 
+    App*app=new App();
+    User*user=new User("Main","Ghar");
+    vector<Restraunt>restraunts=app->search("Dharti");
+    //any other ops
     return 0;
 }
