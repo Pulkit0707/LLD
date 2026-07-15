62899158cf4b915b7baea125f1398c8b43551a09
Pulkit0707 <pulkit0707chawla@gmail.com>
2026-07-15 17:26:19 +0530

    flow with classes connected

--- PaymentGateway/main.cpp
+++ PaymentGateway/main.cpp
@@ -1,125 +1,208 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class PaymentRequest;
+
+//====================== BANK ======================
+
 class Bank{
-    public: 
-    virtual void validate()=0;
-    virtual void transfer()=0;
-    virtual void reportStatus()=0;
+public:
+    virtual void validate() = 0;
+    virtual void transfer() = 0;
+    virtual void reportStatus() = 0;
     virtual ~Bank() = default;
 };
 
-class BOB: public Bank{
-    public:
+class BOB : public Bank{
+public:
     void validate() override{
         cout<<"Validating through BOB"<<endl;
     }
+
     void transfer() override{
-        cout<<"Transferring through BOB"<<endl;
+        cout<<"Money transferred through BOB"<<endl;
     }
+
     void reportStatus() override{
-        cout<<"Reporting status through BOB"<<endl;
+        cout<<"BOB: Payment Successful"<<endl;
     }
 };
 
-class SBI: public Bank{
-    public:
+class SBI : public Bank{
+public:
     void validate() override{
         cout<<"Validating through SBI"<<endl;
     }
+
     void transfer() override{
-        cout<<"Transferring through SBI"<<endl;
+        cout<<"Money transferred through SBI"<<endl;
     }
+
     void reportStatus() override{
-        cout<<"Reporting status through SBI"<<endl;
+        cout<<"SBI: Payment Successful"<<endl;
     }
 };
+
+//====================== FACTORY ======================
 
 class BankFactory{
-    public:
+public:
+
     static Bank* getBank(string bankName){
-        if(bankName=="BOB"){
+
+        if(bankName=="BOB")
             return new BOB();
-        }
-        else if(bankName=="SBI"){
+
+        if(bankName=="SBI")
             return new SBI();
-        }
-        else{
-            return nullptr;
-        }
+
+        return nullptr;
     }
 };
 
-class PaymentStrategy{
-    public:
-    virtual void pay()=0;
-    virtual ~PaymentStrategy() = default;
-};
-
-class PaymentTemplate{
-    Bank* bank;
-    public:
-    PaymentTemplate(Bank* bank): bank(bank){}
-    void payment(){
-        validate();
-        transfer();
-        reportStatus();
-    }
-    void validate(){
-        bank->validate();
-    }
-    virtual void transfer() = 0;
-    void reportStatus(){
-        bank->reportStatus();
-    }
-};
-
-class Paytm: public PaymentStrategy, public PaymentTemplate{
-    public:
-    Paytm(Bank* bank): PaymentTemplate(bank){}
-    void pay() override{
-        cout<<"Paying through Paytm"<<endl;
-    }
-    void transfer() override{
-        cout<<"Transferring through Paytm"<<endl;
-    }
-};
-
-class GooglePay: public PaymentStrategy, public PaymentTemplate{
-    public:
-    GooglePay(Bank* bank): PaymentTemplate(bank){}
-    void pay() override{
-        cout<<"Paying through Google Pay"<<endl;
-    }
-    void transfer() override{
-        cout<<"Transferring through Google Pay"<<endl;
-    }
-};
-
-class PaymentService{
-    PaymentStrategy* strategy;
-    PaymentRequest* request;
-    public:
-    PaymentService(PaymentStrategy* strategy, PaymentRequest* request): strategy(strategy), request(request){}
-    void makePayment(){
-        strategy->pay();
-    }
-};
+//====================== REQUEST ======================
 
 class PaymentRequest{
+
+public:
+
     int amount;
     int from;
     int to;
-    public:
-    PaymentRequest(int amount, int from, int to): amount(amount), from(from), to(to){}
+
+    PaymentRequest(int amount,int from,int to)
+        : amount(amount),from(from),to(to){}
 };
 
+//====================== STRATEGY ======================
+
+class PaymentStrategy{
+
+public:
+
+    virtual void pay(PaymentRequest* request)=0;
+
+    virtual ~PaymentStrategy()=default;
+};
+
+//====================== TEMPLATE ======================
+
+class PaymentTemplate{
+
+protected:
+
+    Bank* bank;
+
+public:
+
+    PaymentTemplate(Bank* bank):bank(bank){}
+
+    // Template Method
+    void payment(){
+
+        bank->validate();
+
+        transfer();
+
+        bank->reportStatus();
+    }
+
+    virtual void transfer()=0;
+};
+
+//====================== PAYTM ======================
+
+class Paytm : public PaymentStrategy, public PaymentTemplate{
+
+public:
+
+    Paytm(Bank* bank)
+        : PaymentTemplate(bank){}
+
+    void pay(PaymentRequest* request) override{
+
+        cout<<"\n===== PAYTM ====="<<endl;
+
+        cout<<"Amount : "<<request->amount<<endl;
+
+        payment();
+    }
+
+    void transfer() override{
+
+        cout<<"Paytm Processing Payment"<<endl;
+
+        bank->transfer();
+    }
+};
+
+//====================== GOOGLE PAY ======================
+
+class GooglePay : public PaymentStrategy, public PaymentTemplate{
+
+public:
+
+    GooglePay(Bank* bank)
+        : PaymentTemplate(bank){}
+
+    void pay(PaymentRequest* request) override{
+
+        cout<<"\n===== GOOGLE PAY ====="<<endl;
+
+        cout<<"Amount : "<<request->amount<<endl;
+
+        payment();
+    }
+
+    void transfer() override{
+
+        cout<<"Google Pay Processing Payment"<<endl;
+
+        bank->transfer();
+    }
+};
+
+//====================== SERVICE ======================
+
+class PaymentService{
+
+    PaymentStrategy* strategy;
+
+    PaymentRequest* request;
+
+public:
+
+    PaymentService(PaymentStrategy* strategy,
+                   PaymentRequest* request)
+        : strategy(strategy),
+          request(request){}
+
+    void makePayment(){
+
+        strategy->pay(request);
+    }
+};
+
+//====================== MAIN ======================
+
 int main(){
-    Bank* b1=BankFactory::getBank("BOB");
-    Bank* b2=BankFactory::getBank("SBI");
-    PaymentStrategy* p1=new Paytm(b1);
-    PaymentStrategy* p2=new GooglePay(b2);
-    PaymentRequest* request=new PaymentRequest(1000, 1, 2);
-    PaymentService* service1=new PaymentService(p1, request);
-    PaymentService* service2=new PaymentService(p2, request);
+
+    Bank* bob = BankFactory::getBank("BOB");
+    Bank* sbi = BankFactory::getBank("SBI");
+
+    PaymentStrategy* paytm = new Paytm(bob);
+    PaymentStrategy* gpay = new GooglePay(sbi);
+
+    PaymentRequest* request =
+        new PaymentRequest(1000,1,2);
+
+    PaymentService* service1 =
+        new PaymentService(paytm,request);
+
+    PaymentService* service2 =
+        new PaymentService(gpay,request);
+
+    service1->makePayment();
+
+    service2->makePayment();
 }
